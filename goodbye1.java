17c845cbcdc0f80bf80c2f85580152614c076941
Wansdj <simon@earlywarning.no>
2018-06-06 13:03:38 +0200

    modified the class name with capital letter

--- goodbye1.java
+++ goodbye1.java
@@ -1,4 +1,5 @@
-public class goodbye1 {
+//Changed class name to start with capital letter-
+public class Goodbye1 {
     public static void main(String[] args) {
         System.out.println("Goodbye, World");
     }
